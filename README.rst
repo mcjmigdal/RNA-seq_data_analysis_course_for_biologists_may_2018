80c0a1789c3ccd79fce607a6d88346c433233bde
migdal migdal <mcjmigdal@gmail.com>
2018-05-08 15:09:43 +0200

    Finished beta workflow and described usage in README

--- README.rst
+++ README.rst
@@ -5,3 +5,11 @@ starting with data download and finishing at differential expression calling.
 
 The tutorial.rst file covers the first part of our workshops, while workshop.sh
 is basicaly a solution to this part of the workshop (TODO).
+
+If you have downloaded this tutorial from the repository you will have all 
+results files ie. fastqc reports, reads quantifications (**so far**). 
+However you will be missing the raw files which are fastqc's and cDNA fasta.
+You can easly download them by runing workflow.sh script with any argument
+like :bash:`bash workflow.sh 1`. This will tell the script to download the raw
+data even when the raw and ref directories already exists, which I've used 
+as the conditional for the workflow.
--- workflow.sh
+++ workflow.sh
@@ -1,20 +1,42 @@
 # paper - http://dev.biologists.org/content/144/19/3487.long
-Names="A30 B30 C30 A72 B72 C72"
-SRA="SRR6039677 SRR6039675 SRR6039689 SRR6039684 SRR6039688 SRR6039673"
-if [ ! -d raw ]; then
+
+download=$1
+
+# Download reads and reference
+Names=(A30 B30 C30 A48 B48 C48 A72 B72 C72)
+SRA=(SRR6039677 SRR6039675 SRR6039689 SRR6039680 SRR6039692 SRR6039686 SRR6039684 SRR6039688 SRR6039673)
+if [ ! -d raw ] || [ ${download} ]; then
   mkdir raw
   cd raw
-  for sra in ${SRA}; do
-    fastq-dump $sra &
+  for (( i=0; i<${#SRA[*]}; i++ )); do
+    fastq-dump --gzip ${SRA[$i]} && mv ${SRA[$i]}.fastq.gz ${Names[$i]}.fastq.gz &
   done
   cd ..
 fi
 
-if [ ! -d ref ]; then
+if [ ! -d ref ] || [ ${download}  ]; then
   mkdir ref
   cd ref
-  wget ftp://ftp.ensembl.org/pub/release-92/fasta/danio_rerio/dna/Danio_rerio.GRCz11.dna_sm.toplevel.fa.gz &
   wget ftp://ftp.ensembl.org/pub/release-92/fasta/danio_rerio/cdna/Danio_rerio.GRCz11.cdna.all.fa.gz
+  cd ..
 fi
 
+# Check reads quality
+if [ ! -d raw/multiqc_data ]; then
+  cd raw
+  fastqc -t 6 *gz
+  multiqc .
+  cd ..
+fi
 
+# Build salmon index | took me about 2 mins at 516A
+if [ ! -d ref/Danio_rerio ]; then
+  salmon index -t ref/Danio_rerio.GRCz11.cdna.all.fa.gz -i Danio_rerio
+fi
+
+# Quantify reads
+if [ ! -d quants ]; then
+  for fq in raw/*.gz; do
+    salmon quant -i ref/Danio_rerio -l A -r ${fq} -p 6 -o quants/${fq%.fastq.gz}_quant
+  done
+fi
