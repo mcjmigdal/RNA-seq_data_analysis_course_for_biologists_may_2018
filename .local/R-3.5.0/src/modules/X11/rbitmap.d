0db3c2f39b9fc3e140e6ffe09aa802a4af0f7283
mcjmigdal <mcjmigdal@gmail.com>
2018-10-10 10:53:51 -0500

    Divide folders strucutre into two parts of tutorial, rename turorial1

--- .local/R-3.5.0/src/modules/X11/rbitmap.d
+++
@@ -1,3 +0,0 @@
-rbitmap.o: rbitmap.c ../../../src/include/config.h bitmap.h \
- /usr/include/libpng16/png.h /usr/include/libpng16/pnglibconf.h \
- /usr/include/libpng16/pngconf.h ../../../src/include/R_ext/Error.h
--- .local/lib/python3.6/site-packages/multiqc/modules/conpair/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import absolute_import
-
-from .conpair import MultiqcModule
